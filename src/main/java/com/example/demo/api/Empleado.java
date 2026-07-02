18dac9597ddcc4ce4c44c132115449ea23735336
Josema13Cj <josesitocu13@gmail.com>
2026-07-02 03:23:22 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/api/Empleado.java
@@ -0,0 +1,5 @@
+package com.example.demo.api;
+ 
+// record Empleado: id, nombre, departamento y salario
+// Acceso: empleado.id()  |  empleado.nombre()  |  empleado.departamento()  |  empleado.salario()
+record Empleado(int id, String nombre, String departamento, double salario) {}
